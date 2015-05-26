68ab0c8429a1483648910d9802695a1ca16f5466
hua wei <hua.wei@cern.ch>
2015-05-26 03:43:35 -0500

    basic code structure

---
+++ Tool/QCD.cc
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <map>
+#include <cmath>
+#include <set>
+#include <cstdio>
+#include <ctime>
+#include <sstream>
+#include <fstream>
+
+#include <vector>
+
+#include "SusyAnaTools/Tools/samples.h"
+#include "SusyAnaTools/Tools/customize.h"
+
+#include "TStopwatch.h"
+#include "TString.h"
+
+#include "Baseline.h"
+#include "QCD.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TH1D.h"
+#include "THStack.h"
+#include "TLatex.h"
+#include "TLegend.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+
+#include "Math/QuantFuncMathCore.h"
+#include "TMath.h"
+#include "TLorentzVector.h"
+//#include "TROOT.h"
+//#include "TInterpreter.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+  if (argc < 2)
+  {
+    std::cerr <<"Please give 2 arguments " << "runList " << " " << "outputFileName" << std::endl;
+    std::cerr <<" Valid configurations are " << std::endl;
+    std::cerr <<" ./QCD runlist_QCD.txt test.root" << std::endl;
+    return -1;
+  }
+  const char *inputFileList = argv[1];
+  const char *outFileName   = argv[2];
+
+  TChain *fChain = new TChain("stopTreeMaker/AUX");
+  //TChain *fChain = new TChain("AUX");
+
+  if(!FillChain(fChain, inputFileList))
+  {
+    std::cerr << "Cannot get the tree " << std::endl;
+  }
+
+  //clock to monitor the run time
+  size_t t0 = clock();
+
+  NTupleReader tr(fChain);
+  //initialize the type3Ptr defined in the customize.h
+  AnaFunctions::prepareTopTagger();
+  //The passBaselineFunc is registered here
+  tr.registerFunction(&passBaselineFunc);
+  //define my histgram class
+  BaseHistgram myBaseHistgram;
+  myBaseHistgram.BookHistgram(outFileName);
+
+  int nevents_baseline= 0;
+
+  while(tr.getNextEvent())
+  {
+    if(tr.getEvtNum()%20000 == 0) std::cout << tr.getEvtNum() << "\t" << ((clock() - t0)/1000000.0) << std::endl;
+
+    bool passBaseline=tr.getVar<bool>("passBaseline");
+    if (passBaseline)
+    {
+      ++nevents_baseline;
+    }
+
+    //bool passBaseline_nolepveto=tr.getVar<bool>("passBaseline_nolepveto");
+    //if (passBaseline_nolepveto)
+    //{
+      //myAccRecoIsoEffs.nevents_sel_base++;
+    //}//baseline, nolepveto
+  }//end of first loop
+
+  //write into histgram
+  (myBaseHistgram.oFile)->Write();
+  
+  //const double ttbarCrossSection=806.1;
+  //const double lumi=1000.0;
+  //const double ntoteventsttbar=25446993.0;
+  //std::cout << "nevents_muonCS = " << nevents_muonCS << std::endl;
+  //std::cout << "nevents_muonCS_norm (1fb-1) = " << nevents_muonCS*ttbarCrossSection*lumi/ntoteventsttbar << std::endl;
+  //std::cout << "nevents_baseline = " << nevents_baseline << std::endl;
+  //std::cout << "nevents_baseline_ref = " << nevents_baseline_ref << std::endl;
+  //std::cout << "nevents_baseline_norm (1fb-1) = " << nevents_baseline*ttbarCrossSection*lumi/ntoteventsttbar << std::endl;
+  return 0;
+}
+
---
+++ Tool/QCD.h
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "TChain.h"
+
+#include "Math/QuantFuncMathCore.h"
+#include "TMath.h"
+
+#include "SusyAnaTools/Tools/NTupleReader.h"
+
+//############finish the definition of class AccRecoEffs######################
+
+class BaseHistgram
+{
+ public:
+  void BookHistgram(const char *);
+
+  TFile *oFile;
+  TH1D *h_b_all_MET;
+  TH1D *h_b_baseline_nMuons, *h_b_baseline_njets, *h_b_baseline_nbjetsCSVM, *h_b_baseline_bestTopMass, *h_b_baseline_MET, *h_b_baseline_jetpt2, *h_b_baseline_jetpt4, *h_b_baseline_jet1_met_phi_diff, *h_b_baseline_jet2_met_phi_diff, *h_b_baseline_jet3_met_phi_diff;
+  TH1D *h_b_acc_njets, *h_b_acc_nbjetsCSVM, *h_b_acc_bestTopMass, *h_b_acc_MET, *h_b_acc_jetpt2, *h_b_acc_jetpt4, *h_b_acc_jet1_met_phi_diff, *h_b_acc_jet2_met_phi_diff, *h_b_acc_jet3_met_phi_diff;
+  TH1D *h_b_reco_nMuons, *h_b_reco_njets, *h_b_reco_nbjetsCSVM, *h_b_reco_bestTopMass, *h_b_reco_MET, *h_b_reco_jetpt2, *h_b_reco_jetpt4, *h_b_reco_jet1_met_phi_diff, *h_b_reco_jet2_met_phi_diff, *h_b_reco_jet3_met_phi_diff;
+};
+
+void BaseHistgram::BookHistgram(const char *outFileName)
+{
+  oFile = new TFile(outFileName, "recreate");
+
+  h_b_all_MET = new TH1D("h_b_all_MET","",1000,0,1000);
+
+  h_b_baseline_nMuons = new TH1D("h_b_baseline_nMuons","",10,0,10);
+  h_b_baseline_njets = new TH1D("h_b_baseline_njets","",10,0,10);
+  h_b_baseline_nbjetsCSVM = new TH1D("h_b_baseline_nbjetsCSVM","",10,0,10);
+  h_b_baseline_bestTopMass = new TH1D("h_b_baseline_bestTopMass","",1000,0,500);
+  h_b_baseline_MET = new TH1D("h_b_baseline_MET","",1000,0,1000);
+  h_b_baseline_jetpt4 = new TH1D("h_b_baseline_jetpt4","",1000,0,1000);
+  h_b_baseline_jetpt2 = new TH1D("h_b_baseline_jetpt2","",1000,0,1000);
+  h_b_baseline_jet1_met_phi_diff = new TH1D("h_b_baseline_jet1_met_phi_diff","",1000,-5,5);
+  h_b_baseline_jet2_met_phi_diff = new TH1D("h_b_baseline_jet2_met_phi_diff","",1000,-5,5);
+  h_b_baseline_jet3_met_phi_diff = new TH1D("h_b_baseline_jet3_met_phi_diff","",1000,-5,5);
+
+  h_b_acc_njets = new TH1D("h_b_acc_njets","",10,0,10);
+  h_b_acc_nbjetsCSVM = new TH1D("h_b_acc_nbjetsCSVM","",10,0,10);
+  h_b_acc_bestTopMass = new TH1D("h_b_acc_bestTopMass","",1000,0,500);
+  h_b_acc_MET = new TH1D("h_b_acc_MET","",1000,0,1000);
+  h_b_acc_jetpt4 = new TH1D("h_b_acc_jetpt4","",1000,0,1000);
+  h_b_acc_jetpt2 = new TH1D("h_b_acc_jetpt2","",1000,0,1000);
+  h_b_acc_jet1_met_phi_diff = new TH1D("h_b_acc_jet1_met_phi_diff","",1000,-5,5);
+  h_b_acc_jet2_met_phi_diff = new TH1D("h_b_acc_jet2_met_phi_diff","",1000,-5,5);
+  h_b_acc_jet3_met_phi_diff = new TH1D("h_b_acc_jet3_met_phi_diff","",1000,-5,5);
+
+  h_b_reco_nMuons = new TH1D("h_b_reco_nMuons","",10,0,10);
+  h_b_reco_njets = new TH1D("h_b_reco_njets","",10,0,10);
+  h_b_reco_nbjetsCSVM = new TH1D("h_b_reco_nbjetsCSVM","",10,0,10);
+  h_b_reco_bestTopMass = new TH1D("h_b_reco_bestTopMass","",1000,0,500);
+  h_b_reco_MET = new TH1D("h_b_reco_MET","",1000,0,1000);
+  h_b_reco_jetpt4 = new TH1D("h_b_reco_jetpt4","",1000,0,1000);
+  h_b_reco_jetpt2 = new TH1D("h_b_reco_jetpt2","",1000,0,1000);
+  h_b_reco_jet1_met_phi_diff = new TH1D("h_b_reco_jet1_met_phi_diff","",1000,-5,5);
+  h_b_reco_jet2_met_phi_diff = new TH1D("h_b_reco_jet2_met_phi_diff","",1000,-5,5);
+  h_b_reco_jet3_met_phi_diff = new TH1D("h_b_reco_jet3_met_phi_diff","",1000,-5,5);
+}
+
+//Fill chain from txt file
+bool FillChain(TChain *chain, const TString &inputFileList)
+{
+  ifstream infile(inputFileList, ifstream::in);
+  std::string buffer;
+
+  if(!infile.is_open())
+  {
+    std::cerr << "** ERROR: Can't open '" << inputFileList << "' for input" << std::endl;
+    return false;
+  }
+
+  std::cout << "TreeUtilities : FillChain " << std::endl;
+  while(1)
+  {
+    infile >> buffer;
+    if(!infile.good()) break;
+    //std::cout << "Adding tree from " << buffer.c_str() << std::endl;                                                              
+    chain->Add(buffer.c_str());
+  }
+  std::cout << "No. of Entries in this tree : " << chain->GetEntries() << std::endl;
+  return true;
+}
+
+//##########functions to calculate Delta_R and Delta Phi###############
+double DeltaPhi(double phi1, double phi2) 
+{
+  double result = phi1 - phi2;
+  while (result > M_PI)    result -= 2 * M_PI;
+  while (result <= -M_PI)  result += 2 * M_PI;
+  return result;
+}
+
+double DeltaR(double eta1, double phi1, double eta2, double phi2) 
+{
+  double deta = eta1 - eta2;
+  double dphi = DeltaPhi(phi1, phi2);
+  return std::sqrt(deta*deta + dphi*dphi);
+}
