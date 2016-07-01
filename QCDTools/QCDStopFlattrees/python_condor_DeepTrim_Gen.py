609c312a8ccd6b6fc7af35ece582de3a17c70de2
hua wei <hua.wei@cern.ch>
2016-07-01 06:07:54 -0500

    add tools for condor jobs

---
+++ QCDTools/QCDStopFlattrees/python_condor_DeepTrim_Gen.py
@@ -0,0 +1,67 @@
+import sys
+import glob, os
+
+def PrintCondorHeaderLine():
+  print("universe = vanilla")
+  print("request_disk   = 50 GB")
+  print("request_memory = 1.5 GB")
+  print("executable = $ENV(CMSSW_BASE)/src/QCD/QCDTools/goDeepTrim.sh")
+  print("should_transfer_files = YES")
+  print("#when_to_transfer_output = ON_EXIT")
+  print ""
+
+def PrintTransferFileLine(directory, sampletype, isfirst, islast):
+  if(isfirst):
+    sys.stdout.write('transfer_input_files = $ENV(CMSSW_BASE)/src/QCD/QCDTools/DeepTrim, $ENV(CMSSW_BASE)/src/QCD/QCDTools/NTuple_DeepTrim.py, $ENV(CMSSW_BASE)/src/QCD/QCDTools/goDeepTrim.sh, ')
+  for dirname, dirnames, filenames in os.walk(directory):
+    for filename in filenames:
+      if ( sampletype in filename ):
+        sys.stdout.write('$ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/2016ICHEPMCTxt/' + filename + ', ')
+      else:
+        continue
+  if(islast):
+    print ""
+    print ""
+
+def PrintCondorLogLine():
+  print ("Output = $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/res/Trim_$(Process).stdout")
+  print ("Error = $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/res/Trim_$(Process).stderr")
+  print ("Log = $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/res/Trim_$(Process).log")
+  print ("notify_user = hua.wei@cern.ch")
+  print ""
+
+def PrintCondorSubmitLine(directory, sampletype):
+  print("#### "+ sampletype +" ####")
+  print ""
+  for dirname, dirnames, filenames in os.walk(directory):
+    for filename in filenames:
+      if ( sampletype in filename ):
+        print ("arguments = $ENV(CMSSW_BASE) " + filename)
+        print ("Queue")
+        print ""
+      else:
+        continue
+
+d = "/uscms_data/d3/hwei/stop/QCD/CMSSW_8_0_12/src/QCD/QCDTools/QCDStopFlattrees/2016ICHEPMCTxt"
+runtype = sys.argv[1]
+print ("#The valid run types for DeepTrim are Data, LL! While the current run type is : " + runtype)
+
+if(runtype == "Data"):
+  PrintCondorHeaderLine()
+  print("##transfer file list for " + runtype + " samples")
+  PrintTransferFileLine(d, "HTMHT", True, True)
+  PrintCondorLogLine()
+  PrintCondorSubmitLine(d, "HTMHT")
+
+elif(runtype == "LL"):
+  PrintCondorHeaderLine()
+  print("##transfer file list for " + runtype + " samples")
+  PrintTransferFileLine(d, "TTJets_", True, False)
+  PrintTransferFileLine(d, "WJetsToLNu_HT-", False, False)
+  PrintTransferFileLine(d, "ST_tW_", False, True)
+  PrintCondorLogLine()
+  PrintCondorSubmitLine(d, "TTJets_")
+  PrintCondorSubmitLine(d, "WJetsToLNu_HT-")
+  PrintCondorSubmitLine(d, "ST_tW_")
+else:
+  print ("#Invalid run type for DeepTrim! What the fuck is going on ??!!")
---
+++ QCDTools/QCDStopFlattrees/python_condor_QCDTFTrimAndSlim_Gen.py
@@ -0,0 +1,86 @@
+import sys
+import glob, os
+
+def PrintCondorHeaderLine():
+  print("universe = vanilla")
+  print("request_disk   = 50 GB")
+  print("request_memory = 1.5 GB")
+  print("executable = $ENV(CMSSW_BASE)/src/QCD/QCDTools/goQCDTFTrimAndSlim.sh")
+  print("should_transfer_files = YES")
+  print("#when_to_transfer_output = ON_EXIT")
+  print ""
+
+def PrintTransferFileLine(directory, sampletype, isfirst, islast):
+  if(isfirst):
+    sys.stdout.write('transfer_input_files = $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDTFTrimAndSlim_LLHadTau, $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDTFTrimAndSlim_Zinv, $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDTFTrimAndSlim_QCD, $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDTFTrimAndSlim_TTZ, $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDTFTrimAndSlim_Data, $ENV(CMSSW_BASE)/src/QCD/QCDTools/NTuple_QCDTFTrimAndSlim.py, $ENV(CMSSW_BASE)/src/QCD/QCDTools/goQCDTFTrimAndSlim.sh, ')
+  for dirname, dirnames, filenames in os.walk(directory):
+    for filename in filenames:
+      if ( sampletype in filename ):
+        sys.stdout.write('$ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/2016ICHEPMCTxt/' + filename + ', ')
+      else:
+        continue
+  if(islast):
+    print ""
+    print ""
+
+def PrintCondorLogLine():
+  print ("Output = $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/res/Trim_$(Process).stdout")
+  print ("Error = $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/res/Trim_$(Process).stderr")
+  print ("Log = $ENV(CMSSW_BASE)/src/QCD/QCDTools/QCDStopFlattrees/res/Trim_$(Process).log")
+  print ("notify_user = hua.wei@cern.ch")
+  print ""
+
+def PrintCondorSubmitLine(directory, sampletype, command):
+  print("#### "+ sampletype +" ####")
+  print ""
+  for dirname, dirnames, filenames in os.walk(directory):
+    for filename in filenames:
+      if ( sampletype in filename ):
+        print ("arguments = $ENV(CMSSW_BASE) " + command + " " + filename)
+        print ("Queue")
+        print ""
+      else:
+        continue
+
+d = "/uscms_data/d3/hwei/stop/QCD/CMSSW_8_0_12/src/QCD/QCDTools/QCDStopFlattrees/2016ICHEPMCTxt"
+runtype = sys.argv[1]
+print ("#The valid run types for QCDTFTrimAndSlim are Data, LLHadTau, Zinv, QCD, and TTZ! While the current run type is : " + runtype)
+
+if(runtype == "Data"):
+  PrintCondorHeaderLine()
+  print("##transfer file list for " + runtype + " samples")
+  PrintTransferFileLine(d, "HTMHT", True, True)
+  PrintCondorLogLine()
+  PrintCondorSubmitLine(d, "HTMHT", runtype)
+elif(runtype == "LLHadTau"):
+  PrintCondorHeaderLine()
+  print("##transfer file list for " + runtype + " samples")
+  PrintTransferFileLine(d, "TTJets_", True, False)
+  PrintTransferFileLine(d, "WJetsToLNu_HT-", False, False)
+  PrintTransferFileLine(d, "ST_tW_", False, True)
+  PrintCondorLogLine()
+  PrintCondorSubmitLine(d, "TTJets_", runtype)
+  PrintCondorSubmitLine(d, "WJetsToLNu_HT-", runtype)
+  PrintCondorSubmitLine(d, "ST_tW_", runtype)
+elif(runtype == "Zinv"):
+  PrintCondorHeaderLine()
+  print("##transfer file list for " + runtype + " samples")
+  PrintTransferFileLine(d, "ZJetsToNuNu_HT-", True, True)
+  PrintCondorLogLine()
+  PrintCondorSubmitLine(d, "ZJetsToNuNu_HT-", runtype)
+elif(runtype == "QCD"):
+  PrintCondorHeaderLine()
+  print("##transfer file list for " + runtype + " samples")
+  PrintTransferFileLine(d, "QCD_HT", True, True)
+  PrintCondorLogLine()
+  PrintCondorSubmitLine(d, "QCD_HT", runtype)
+elif(runtype == "TTZ"):
+  PrintCondorHeaderLine()
+  print("##transfer file list for " + runtype + " samples")
+  PrintTransferFileLine(d, "TTZTo", True, False)
+  PrintTransferFileLine(d, "TTWJetsTo", False, True)
+  PrintCondorLogLine()
+  PrintCondorSubmitLine(d, "TTZTo", runtype)
+  PrintCondorSubmitLine(d, "TTWJetsTo", runtype)
+else:
+  print ("#Invalid run type for QCDTFTrimAndSlim! What the fuck is going on ??!!")
