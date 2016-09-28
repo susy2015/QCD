46425977e638d173efd82f8ef0b732bda39086f6
hua wei <hua.wei@cern.ch>
2016-09-28 04:03:21 -0500

    done with SF check root files, writing code now

--- QCDTools/EOSCmd_SampleTag.py
+++ QCDTools/EOSCmd_SampleTag.py
@@ -1,8 +1,10 @@
 import glob, os, sys
 
 d_hadd_source = "/store/group/lpcsusyhad/hua/Skimmed_2015Nov15"
-d_final_target = "/store/group/lpcsusyhad/hua/Skimmed_2015Nov15/QCD_Data_2016ICHEPv9"
-d_final_target = "/store/group/lpcsusyhad/hua/Skimmed_2015Nov15/LL_Data_2016ICHEPv9"
+#d_final_target = "/store/group/lpcsusyhad/hua/Skimmed_2015Nov15/QCD_Data_2016ICHEPv9"
+#d_final_target = "/store/group/lpcsusyhad/hua/Skimmed_2015Nov15/LL_Data_2016ICHEPv9"
+d_final_target = "/store/group/lpcsusyhad/hua/Skimmed_2015Nov15/SFCheckTrim_2016Sep"
+
 MGM = "root://cmseos.fnal.gov/"
 
 sample_tag_list_Data = [
@@ -81,6 +83,28 @@ sample_tag_list_DeepTrim = [
 #"DeepTrimmd_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_ST_tW_antitop_5f_inclusiveDecays_stopFlatNtuples_",
                            ]
 
+sample_tag_list_SFCheckTrim = [
+"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_FINAL_HTMHT-Run2016B-PromptReco-v2_stopFlatNtuples_",
+"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_FINAL_HTMHT-Run2016C-PromptReco-v2_stopFlatNtuples_",
+"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_FINAL_HTMHT-Run2016D-PromptReco-v2_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_TTJets_SingleLeptFromT_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_TTJets_SingleLeptFromT_ext1_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_TTJets_SingleLeptFromTbar_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_TTJets_SingleLeptFromTbar_ext1_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_TTJets_DiLept_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_TTJets_DiLept_ext1_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-400To600_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-400To600_ext1_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-600To800_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-800To1200_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-800To1200_ext1_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-1200To2500_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-1200To2500_ext1_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_WJetsToLNu_HT-2500ToInf_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_ST_tW_top_5f_inclusiveDecays_stopFlatNtuples_",
+#"SFCheckTrimmed_Spring16_80X_Jul_2016_Ntp_v8p0_new_JEC_new_muonID_top_up_ST_tW_antitop_5f_inclusiveDecays_stopFlatNtuples_",
+                           ]
+
 #example of sample tag: SSTrimmed_SMS-T2tt_mStop-850_mLSP-100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_stopFlatNtuples_, always end with "_"
 sample_tag = sys.argv[1]
 
@@ -98,6 +122,8 @@ elif(sample_tag == 'TTZ') :
   sample_tag_list = sample_tag_list_TTZ
 elif(sample_tag == 'DeepTrim') :
   sample_tag_list = sample_tag_list_DeepTrim
+elif(sample_tag == 'SFCheckTrim') :
+  sample_tag_list = sample_tag_list_SFCheckTrim
 else:
   print "wrong run type!"
 
---
+++ QCDTools/SFCheckTrim.cc
@@ -0,0 +1,93 @@
+#include <fstream>
+#include <map>
+#include <iomanip>
+#include <locale>
+#include <sstream>
+#include <stdlib.h>
+
+#include "TTree.h"
+#include "TFile.h"
+#include "TChain.h"
+#include "TString.h"
+#include "TLorentzVector.h"
+#include "TInterpreter.h"
+
+#include "SusyAnaTools/Tools/samples.h"
+#include "SusyAnaTools/Tools/searchBins.h"
+
+#include "SFCheckTrim.h"
+
+int main(int argc, char* argv[])
+{
+  if (argc < 1)
+  {
+    std::cerr <<"Please give 1 argument " << "inputFileName " << std::endl;
+    std::cerr <<"Valid configurations are: " << std::endl;
+    std::cerr <<"./SFCheckTrim root://cmseos.fnal.gov//store/user/lpcsusyhad/Spring15_74X_Oct_2015_Ntp_v2X/QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/QCD_HT500to700_Spring15DR74_Asympt25ns_Ntp_v2/150928_140039/0000/stopFlatNtuples_1.root" << std::endl;
+    return -1;
+  }
+  std::string input_str(argv[1]);
+  std::string trim;
+  //std::string outputpath = "/eos/uscms/store/group/lpcsusyhad/hua/Skimmed_2015Nov15/";
+
+  std::string output_str;
+  //here is a little bit tricky when dealing with the slash... need to improve
+  //for all the MC samples
+  //std::string tag = input_str.substr(find_Nth(input_str,10,"/") + 1,find_Nth(input_str,11,"/")-find_Nth(input_str,10,"/")-1);
+  //for all the data samples
+  std::string tag = input_str.substr(find_Nth(input_str,9,"/") + 1,find_Nth(input_str,10,"/")-find_Nth(input_str,9,"/")-1);
+  //std::string tag = input_str.substr(find_Nth(input_str,10,"/") + 1,find_Nth(input_str,11,"/")-find_Nth(input_str,10,"/")-1);
+  std::size_t idpos = input_str.find("stopFlatNtuples");
+  std::string fileid = input_str.substr (idpos);
+
+  output_str = "SFCheckTrimmed_" + tag + "_" + fileid;
+  std::cout << "Output File Name: " << output_str << std::endl;
+
+  TChain *originalTree = new TChain("stopTreeMaker/AUX");
+  originalTree->Add(input_str.c_str());
+  originalTree->SetBranchStatus("*", 1);
+   
+  TFile* output = new TFile((output_str).c_str(), "RECREATE");
+  TDirectory *mydict = output->mkdir("stopTreeMaker");
+  mydict->cd();
+  TTree* selectedTree = originalTree->CloneTree(0);
+
+  const std::string spec = "QCD";
+  myBaselineVessel = new BaselineVessel(spec);
+
+  //use class NTupleReader in the SusyAnaTools/Tools/NTupleReader.h file
+  NTupleReader tr(originalTree);
+  //initialize the type3Ptr defined in the customize.h
+  AnaFunctions::prepareTopTagger();
+  //The passBaseline is registered here
+  tr.registerFunction(&mypassBaselineFunc);
+
+  while(tr.getNextEvent())
+  {
+    double met = tr.getVar<double>("met");
+    double ht = tr.getVar<double>("HT"+spec);
+    int njetspt30 = tr.getVar<int>("cntNJetsPt30"+spec);
+    int nmus = tr.getVar<int>("nMuons_CUT"+spec);
+    int nels = tr.getVar<int>("nElectrons_CUT"+spec);
+
+    bool passSFCheckTrim = false;
+    passSFCheckTrim = ( met > 250 )
+                   && ( ht > 250 )
+                   && ( njetspt30 > 1 )
+                   && ( (nmus==1 && nels == 0) || (nmus==0 && nels == 1) );
+
+    if(passSFCheckTrim) selectedTree->Fill();
+    else continue;
+  }
+  selectedTree->Write();
+  output->Write(); 
+  output->Close();
+
+  if (originalTree) delete originalTree;
+
+  std::string d = "root://cmseos.fnal.gov//store/group/lpcsusyhad/hua/Skimmed_2015Nov15";
+  std::system(("xrdcp " + output_str + " " + d).c_str());
+  std::system(("rm " + output_str).c_str());
+
+  return 0;
+}
