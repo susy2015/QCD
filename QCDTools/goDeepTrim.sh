5a5a51821b25c7caced2bd689614c71ba604db73
hua wei <hua.wei@cern.ch>
2017-02-07 07:46:41 -0600

    condor fix and v12 update

--- QCDTools/goDeepTrim.sh
+++ QCDTools/goDeepTrim.sh
@@ -2,10 +2,17 @@
 
 export PATH=${PATH}:/cvmfs/cms.cern.ch/common
 export CMS_PATH=/cvmfs/cms.cern.ch
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$1/src/QCD/QCDTools/obj:$1/src/TopTagger/TopTagger/test:$1/src/opencv/lib
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$1/src/QCD/QCDTools/obj:$1/src/TopTagger/TopTagger/test:$1/src/opencv/lib:$1/lib/slc6_amd64_gcc530
 
-cd $1/src
+tar -xzf CMSSW_8_0_23.tar.gz
+cd CMSSW_8_0_23
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${_CONDOR_SCRATCH_DIR}/CMSSW_8_0_23/src/SusyAnaTools/Tools/obj:${_CONDOR_SCRATCH_DIR}/CMSSW_8_0_23/src/TopTagger/TopTagger/test:${_CONDOR_SCRATCH_DIR}/CMSSW_8_0_23/src/opencv/lib:${_CONDOR_SCRATCH_DIR}/CMSSW_8_0_23/lib/slc6_amd64_gcc530
+# cmsenv
 eval `scramv1 runtime -sh`
+
+#cd $1/src
+#eval `scramv1 runtime -sh`
 
 cd ${_CONDOR_SCRATCH_DIR}
 python NTuple_DeepTrim.py $2
