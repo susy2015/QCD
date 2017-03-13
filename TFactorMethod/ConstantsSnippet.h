ce73d8d9f99a1a1ee35e17d87a55e43f15824547
hua wei <hua.wei@cern.ch>
2017-03-13 12:21:48 -0500

    update file list, move to ttjets ht for qcd, also update to latest zinv constants

--- TFactorMethod/ConstantsSnippet.h
+++ TFactorMethod/ConstantsSnippet.h
@@ -39,13 +39,13 @@ const double htbins_ext_edge[HT_Ext_BINS+1] = {300.0,2000.0};
 //const double zinv_NJetRweightingFactor[8] = {0.974553,1.03047,0.957998,0.806491,0.719326,0.86521,0.844148,0.599463};//v8, 7.6fb-1
 //const double zinv_NJetRweightingFactor[8] = {1.02845,1.08559,1.06879,0.922173,0.871796,0.99674,0.993756,0.539612};//v8 MC and v9 data, 12.9 fb-1
 //const double zinv_NJetRweightingFactor[8] = {      0,      0,      0,0.715566,0.630464,0.59214,0.523173,0.406519};//v11 MC Data, 36.4 fb-1, all 2016 runs
-//                                  NJets =      1        2       3        4        5       6     >=7
-const double zinv_NJetRweightingFactor[ZINVNJETS_BINS] = {    0,       0,      0,0.422219,0.792718,0.77479,0.350352};//v13 Data, 35.87 fb-1, all 2016 runs
+//                                               NJets =      1        2       3        4        5       6     >=7
+const double zinv_NJetRweightingFactor[ZINVNJETS_BINS] = {    0,       0,      0,0.535176,0.924481,0.91038,0.491111};//v13 Data, 35.87 fb-1, all 2016 runs
 //const double zinv_RNorm = 0.828;//4.0fb-1, v6 and v7
 //const double zinv_RNorm = 0.868;//7.6fb-1, v8
 //const double zinv_RNorm = 0.783;//v8 MC and v9 data, 12.9 fb-1
 //const double zinv_RNorm = 0.963;//v11 MC Data, 36.4 fb-1
-const double zinv_RNorm = 1.070;//v13 Data, 35.87 fb-1
+const double zinv_RNorm = 1.067;//v13 Data, 35.87 fb-1
 
 //Constants from single muon CS correction factor for MCDriven method
 //2016 ICHEP v9 final result
