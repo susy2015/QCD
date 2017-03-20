ee106991ad38658ee6014b13befb71d9a559dfa4
hua wei <hua.wei@cern.ch>
2017-03-20 09:42:01 -0500

    ready for app

--- TFactorMethod/QCDHardCodedInput/TFactorsfromDataHeader.h
+++ TFactorMethod/QCDHardCodedInput/TFactorsfromDataHeader.h
@@ -1,4 +1,4 @@
-  const double head_QCDTFactorData[2] = {0.338008,0.0201525};
+  const double head_QCDTFactorData[2] = {0.348168,0.0195379};
   const double head_QCDTFactorData_err[2] = {0.0234358,0.0245523};
-  const double head_ext_QCDTFactorData[1] = {0.189665};
+  const double head_ext_QCDTFactorData[1] = {0.199918};
   const double head_ext_QCDTFactorData_err[1] = {0.116896};
